3997bcb466ae224936dd392af047696e8db3b4de
prathameshkalburgi <46131266+prathameshkalburgi@users.noreply.github.com>
2023-03-22 19:05:50 +0530

    Delete src directory

--- src/main/java/com/example/EVproject/EVCHragingStation/EVRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EVproject.EVCHragingStation;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EVRepository extends JpaRepository<EVChargingStation, Integer> {
-
-}
--- src/main/java/com/example/EVproject/requests/AddStationRequest.java
+++
@@ -1,4 +0,0 @@
-package com.example.EVproject.requests;
-
-public class AddStationRequest {
-}
